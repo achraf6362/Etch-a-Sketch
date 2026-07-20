77ac41d5073ab8e7344407e2490b2eac104f9378
A <123079127+achraf6362@users.noreply.github.com>
2026-07-20 19:36:39 +0100

    Add Etch-a-Sketch

--- script.js
+++ script.js
@@ -1,7 +1,51 @@
-let grid = document.querySelector('.grid');
+let grid = document.querySelector('#grid');
+let change = document.querySelector('#change');
+let clear = document.querySelector('#clear');
+let gridSize = 16;
 
-for (let i = 0; i < 16; i++) {
-  let child = document.createElement('div');
-  child.classList.add('child');
-  grid.appendChild(child);
+function play() {
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let child = document.createElement('div');
+    child.classList.add('child');
+    grid.appendChild(child);
+  }
 }
+
+function playAgain() {
+  let num = +prompt('Enter a number:', 16);
+
+  if (num > 100) {
+    num = 100;
+  } else if (num < 2) {
+    num = 2;
+  }
+
+  gridSize = num;
+  document.documentElement.style.setProperty('--grid-size', num)
+  grid.innerHTML = '';
+
+  play();
+}
+
+/* [...grid.children].forEach(child => {
+  child.addEventListener('mouseenter', (e) => {
+    let current = e.currentTarget;
+    current.classList.add('hovered');
+  });
+}); */  
+
+grid.addEventListener('mouseover', (e) => {
+  if (e.target !== grid) {
+  e.target.classList.add('hovered');
+  }
+});
+
+change.addEventListener('click', playAgain);
+
+clear.addEventListener('click', () => {
+  [...grid.children].forEach(child => {
+    child.classList.remove('hovered');
+  });
+});
+
+play();
